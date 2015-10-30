95d25190460e7a72dde2c5b416acbd4a14dcaa8e
Hang Zhang <hangzh@microsoft.com>
2015-10-30 00:16:34 -0700

    add comments to the sql file

--- Misc/RSQL/create-db-tb-upload-data.sql
+++ Misc/RSQL/create-db-tb-upload-data.sql
@@ -1,9 +1,11 @@
 DECLARE @db_name varchar(255), @tb_name varchar(255), @path_to_data varchar(255)
 DECLARE @create_db_template varchar(max), @create_tb_template varchar(max), @upload_data_template varchar(max)
 DECLARE @sql_script varchar(max)
-SET @db_name = 'TaxiNYC_Sample' --Please plug in the database name you want to create
-SET @tb_name = 'nyctaxi_joined_1_percent' --Please plug in the table name you want to create
-SET @path_to_data = 'C:\temp\nyctaxi1pct.csv' --Please plug in path to the data file on the server you want to load to the table
+SET @db_name = '<database name>' --Please plug in the database name you want to create
+
+SET @tb_name = '<table name>' --Please plug in the table name you want to create
+
+SET @path_to_data = '<path to the downloaded data file>' --Please plug in the path to the data file to be loaded on the server
 SET @create_db_template = 'create database {db_name}'
 SET @create_tb_template = '
 use {db_name}
@@ -43,13 +45,16 @@ SET @upload_data_template = 'BULK INSERT {db_name}.dbo.{tb_name}
    	WITH ( FIELDTERMINATOR ='','', FIRSTROW = 2, ROWTERMINATOR = ''\n'' )
 '
 
+-- Create database
 SET @sql_script = REPLACE(@create_db_template, '{db_name}', @db_name)
 EXECUTE(@sql_script)
 
+-- Create table
 SET @sql_script = REPLACE(@create_tb_template, '{db_name}', @db_name)
 SET @sql_script = REPLACE(@sql_script, '{tb_name}', @tb_name)
 EXECUTE(@sql_script)
 
+-- Upload data from a local file on the server to the table
 SET @sql_script = REPLACE(@upload_data_template, '{db_name}', @db_name)
 SET @sql_script = REPLACE(@sql_script, '{tb_name}', @tb_name)
 SET @sql_script = REPLACE(@sql_script, '{path_to_data}', @path_to_data)
