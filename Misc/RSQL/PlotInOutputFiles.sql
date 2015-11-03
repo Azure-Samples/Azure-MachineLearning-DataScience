f6e28de48f2d9cca08664e051bfe911b8d447d74
hangzh-msft <hangzh@microsoft.com>
2015-11-02 16:48:42 -0800

    add lines to create the output directory in sql server if it does not exist and make the output file name random with some fix leading name

--- Misc/RSQL/PlotInOutputFiles.sql
+++ Misc/RSQL/PlotInOutputFiles.sql
@@ -13,27 +13,33 @@ IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'PlotInOutputFi
 GO
 
 
-CREATE PROCEDURE [dbo].[PlotInOutputFiles]
+create PROCEDURE [dbo].[PlotInOutputFiles]
 AS
 BEGIN
   SET NOCOUNT ON;
   DECLARE @query nvarchar(max) =
-  N'SELECT tipped, tip_amount, fare_amount FROM [TaxiNYC_Sample].[dbo].[nyctaxi_joined_1_percent]'
+  N'SELECT cast(tipped as int) as tipped, tip_amount, fare_amount FROM [TaxiNYC_Sample].[dbo].[nyctaxi_joined_1_percent]'
   EXECUTE sp_execute_external_script @language = N'R',
                                      @script = N'
 
 # Set output directory for files
 # Prior to plotting ensure there are no files with same file names as the out files below in the above directory.
-setwd(''C:\\temp\\Plots\\'');
+mainDir <- ''C:\\temp\\plots\\''
+dir.create(file.path(mainDir), recursive = TRUE, showWarnings = FALSE)
+setwd(mainDir);
 
 # Open a jpeg file and output histogram of tipped variable in that file.
-jpeg(filename=''rHistogram_Tipped.jpg'');
+dest_filename = tempfile(pattern = ''rHistogram_Tipped_'', tmpdir = getwd())
+dest_filename = paste(dest_filename, ''.jpg'',sep="")
+jpeg(filename=dest_filename);
 hist(InputDataSet$tipped, col = ''lightgreen'', xlab=''Tipped'', ylab = ''Counts'', main = ''Histogram, Tipped'');
 dev.off();
 
 # Open a pdf file and output histograms of tip amount and fare amount. 
 # Outputs two plots in one row
-pdf(file=''rHistograms_Tip_and_Fare_Amount.pdf'', height=4, width=7);
+dest_filename = tempfile(pattern = ''rHistograms_Tip_and_Fare_Amount_'', tmpdir = getwd())
+dest_filename = paste(dest_filename, ''.pdf'',sep="")
+pdf(file=dest_filename, height=4, width=7);
 par(mfrow=c(1,2));
 hist(InputDataSet$tip_amount, col = ''lightgreen'', xlab=''Tip amount ($)'', ylab = ''Counts'', main = ''Histogram, Tip amount'', xlim = c(0,40), 100);
 hist(InputDataSet$fare_amount, col = ''lightgreen'', xlab=''Fare amount ($)'', ylab = ''Counts'', main = ''Histogram, Fare amount'', xlim = c(0,100), 100);
@@ -41,7 +47,9 @@ dev.off();
 
 # Open a pdf file and output an xyplot of tip amount vs. fare amount. This uses the lattice package in Rlibrary(lattice);
 # Only 10,000 sampled observations are plotted here, otherwise file is large.
-pdf(file=''rXYPlots_Tip_vs_Fare_Amount.pdf'', height=4, width=4);
+dest_filename = tempfile(pattern = ''rXYPlots_Tip_vs_Fare_Amount_'', tmpdir = getwd())
+dest_filename = paste(dest_filename, ''.pdf'',sep="")
+pdf(file=dest_filename, height=4, width=4);
 plot(tip_amount ~ fare_amount, data = InputDataSet[sample(nrow(InputDataSet), 10000), ], ylim = c(0,50), xlim = c(0,150), cex=.5, pch=19, col=''darkgreen'',  main = ''Tip amount by Fare amount'', xlab=''Fare Amount ($)'', ylab = ''Tip Amount ($)''); 
 dev.off();
 ',
@@ -49,5 +57,3 @@ dev.off();
 END
 
 GO
-
-
