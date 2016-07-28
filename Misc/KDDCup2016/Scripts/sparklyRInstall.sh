da9952eba5b04c18399c07d1ff69bd25a54ef997
Debraj GuhaThakurta <deguhath@users.noreply.github.com>
2016-07-27 23:03:24 -0700

    Delete sparklyRInstall.sh

--- Misc/KDDCup2016/Scripts/sparklyRInstall.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get install libcurl4-openssl-dev
-cd /usr/lib64/MRO-for-MRS-8.0.3/R-3.2.2/lib/R
-sudo rm -r sparklyr sparkapi rprojroot dplyr Rcpp DBI config tibble devtools rmarkdown
-cd
-sudo R --vanilla --quiet  <  /home/remoteuser/Scripts/github_installs.R
