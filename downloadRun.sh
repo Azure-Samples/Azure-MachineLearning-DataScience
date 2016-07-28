09ea0f0e3a95cc00371b32d4969517e129d00ab6
Debraj GuhaThakurta <deguhath@users.noreply.github.com>
2016-07-27 23:47:46 -0700

    Add files via upload

---
+++ downloadRun.sh
@@ -0,0 +1,40 @@
+## Download scripts and code files into directories
+cd /home/remoteuser
+mkdir Code  
+mkdir Code/MRS Code/sparklyr
+
+cd /home/remoteuser
+wget https://raw.githubusercontent.com/Azure/Azure-MachineLearning-DataScience/master/Misc/KDDCup2016/Scripts/downloadRun.sh
+wget https://raw.githubusercontent.com/Azure/Azure-MachineLearning-DataScience/master/Misc/KDDCup2016/Scripts/github_installs.R
+chmod +x downloadRun.sh
+
+cd  /home/remoteuser/Code/sparklyr
+wget https://raw.githubusercontent.com/Azure/Azure-MachineLearning-DataScience/master/Misc/KDDCup2016/Code/SparkR/SparklyR_NYCTaxi.Rmd
+wget https://raw.githubusercontent.com/Azure/Azure-MachineLearning-DataScience/master/Misc/KDDCup2016/Code/SparkR/SparklyR_NYCTaxi.html
+
+## Install packages, remove older version of packages prior to installation
+cd /home/remoteuser
+sudo apt-get -y -qq install libcurl4-openssl-dev
+
+cd /usr/lib64/MRO-for-MRS-8.0.3/R-3.2.2/lib/R/library
+if [[ -d sparklyr ]]; then sudo rm -Rf sparklyr; fi;
+if [[ -d sparkapi ]]; then sudo rm -Rf sparkapi; fi;
+if [[ -d rprojroot ]]; then sudo rm -Rf rprojroot; fi;
+if [[ -d dplyr ]]; then sudo rm -Rf dplyr; fi;
+if [[ -d Rcpp ]]; then sudo rm -Rf Rcpp; fi;
+if [[ -d DBI ]]; then sudo rm -Rf DBI; fi;
+if [[ -d config ]]; then sudo rm -Rf config; fi;
+if [[ -d tibble ]]; then sudo rm -Rf tibble; fi;
+if [[ -d devtools ]]; then sudo rm -Rf devtools; fi;
+if [[ -d rmarkdown ]]; then sudo rm -Rf rmarkdown; fi;
+if [[ -d knitr ]]; then sudo rm -Rf knitr; fi;
+
+cd /home/remoteuser/R/x86_64-pc-linux-gnu-library/3.2
+if [[ -d rmarkdown ]]; then sudo rm -Rf rmarkdown; fi;
+
+# Call R file to install packages
+cd /home/remoteuser
+sudo R --vanilla --quiet  <  /home/remoteuser/github_installs.R
+
+## Set working directory
+cd /home/remoteuser/Code
---
+++ github_installs.R
@@ -0,0 +1,18 @@
+.libPaths("/usr/lib64/MRO-for-MRS-8.0.3/R-3.2.2/lib/R/library")
+setwd("/usr/lib64/MRO-for-MRS-8.0.3/R-3.2.2/lib/R/library")
+
+install.packages("devtools", repos='http://cran.us.r-project.org')
+library(devtools)
+
+options(unzip = 'internal')
+devtools::install_github("hadley/tibble");
+devtools::install_github("rstudio/config");
+devtools::install_github("rstats-db/DBI");
+install.packages("Rcpp", repos='http://cran.us.r-project.org')
+devtools::install_github("hadley/dplyr");
+devtools::install_github("krlmlr/rprojroot");
+devtools::install_github("rstudio/sparklyr");
+install.packages("rmarkdown", repos='http://cran.us.r-project.org')
+install.packages("ggplot2", repos='http://cran.us.r-project.org')
+install.packages("gridExtra", repos='http://cran.us.r-project.org')
+install.packages("knitr", repos='http://cran.us.r-project.org')
