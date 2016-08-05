1990df245c6804ef1b9da756f75cbad7588f7e10
Wei Guo <guowei123@gmail.com>
2016-08-05 13:24:00 -0700

    Update SeedGroupProjectTemplateandUtilities.sh

--- Misc/TDSP/SeedGroupProjectTemplateandUtilities.sh
+++ Misc/TDSP/SeedGroupProjectTemplateandUtilities.sh
@@ -1,17 +1,17 @@
-			#Copy ProjectTemplate folder to GroupProjectTemplate folder
-			src="ProjectTemplate"
-			dest="GroupProjectTemplate"
-			SourceDirectory=$PWD/$src
-			DestinationDirectory=$PWD/$dest
-			cd $SourceDirectory
-			git archive HEAD --format=tar | (cd $DestinationDirectory; tar xvf -)
-			cd ../
+#Copy ProjectTemplate folder to GroupProjectTemplate folder
+src="ProjectTemplate"
+dest="GroupProjectTemplate"
+SourceDirectory=$PWD/$src
+DestinationDirectory=$PWD/$dest
+cd $SourceDirectory
+git archive HEAD --format=tar | (cd $DestinationDirectory; tar xvf -)
+cd ../
 
-			# Copy Utilities folder to GroupUtilities folder
-			src="Utilities"
-			dest="GroupUtilities"
-			SourceDirectory=$PWD/$src
-			DestinationDirectory=$PWD/$dest
-			cd $SourceDirectory
-			git archive HEAD --format=tar | (cd $DestinationDirectory; tar xvf -)
-			cd ../ 
+# Copy Utilities folder to GroupUtilities folder
+src="Utilities"
+dest="GroupUtilities"
+SourceDirectory=$PWD/$src
+DestinationDirectory=$PWD/$dest
+cd $SourceDirectory
+git archive HEAD --format=tar | (cd $DestinationDirectory; tar xvf -)
+cd ../ 
