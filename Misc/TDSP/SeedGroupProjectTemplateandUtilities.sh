c619c338e2739d2ec22753d64fba3d7e198358c4
Wei Guo <guowei123@gmail.com>
2016-08-05 13:00:58 -0700

    Create SeedGroupProjectTemplateandUtilities.sh

---
+++ Misc/TDSP/SeedGroupProjectTemplateandUtilities.sh
@@ -0,0 +1,17 @@
+			#Copy ProjectTemplate folder to GroupProjectTemplate folder
+			src="ProjectTemplate"
+			dest="GroupProjectTemplate"
+			SourceDirectory=$PWD/$src
+			DestinationDirectory=$PWD/$dest
+			cd $SourceDirectory
+			git archive HEAD --format=tar | (cd $DestinationDirectory; tar xvf -)
+			cd
+
+			# Copy Utilities folder to GroupUtilities folder
+			src="Utilities"
+			dest="GroupUtilities"
+			SourceDirectory=$PWD/$src
+			DestinationDirectory=$PWD/$dest
+			cd $SourceDirectory
+			git archive HEAD --format=tar | (cd $DestinationDirectory; tar xvf -)
+			cd 
