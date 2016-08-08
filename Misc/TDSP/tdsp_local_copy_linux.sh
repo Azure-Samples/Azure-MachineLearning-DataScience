5dcdd6c4b7b9804b955143c3abaa064362dcd675
Wei Guo <guowei123@gmail.com>
2016-08-08 14:22:06 -0700

    Create tdsp_local_copy_linux.sh

---
+++ Misc/TDSP/tdsp_local_copy_linux.sh
@@ -0,0 +1,20 @@
+#Copy ProjectTemplate and Utilities folder to GroupProjectTemplate and GroupUtilities folder
+
+function TDSP_local_copy_linux{
+
+echo -n "What is your source folder?"
+read src
+echo -n "What is your destination folder?"
+read dest
+
+SourceDirectory=$PWD/$src
+DestinationDirectory=$PWD/$dest
+cd $SourceDirectory
+git archive HEAD --format=tar | (cd $DestinationDirectory; tar xvf -)
+cd ../
+
+}
+#ProjectTemplate --> GroupProjectTemplate
+TDSP_local_copy_linux
+#Utilities --> GroupUtilities
+TDSP_local_copy_linux
