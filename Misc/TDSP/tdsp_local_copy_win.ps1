b24d231d3fa388a45cfab098bcbb9d4725ad016e
Hang Zhang <hangzh@microsoft.com>
2016-08-09 18:37:09 -0700

    modified to validate the directories

--- Misc/TDSP/tdsp_local_copy_win.ps1
+++ Misc/TDSP/tdsp_local_copy_win.ps1
@@ -1,24 +1,39 @@
 # Copy ProjectTemplate and Utilities folder to GroupProjectTemplate and GroupUtilities folder
 
-function TDSP_local_copy {
+function TDSP_local_copy ($reponame1, $reponame2) {
+    $prompt1 = 'Please input the local path to '+$reponame1+' (source directory)'
+    $prompt2 = 'Please input the local path to '+$reponame2+' (source directory)'
+    
+    $validpath = $false
+    while(!$validpath) {
+        $src = Read-Host -Prompt $prompt1
+        if (Test-Path $src)
+        {
+            $validpath = $true
+        }
+    }
 
-$src = Read-Host -Prompt 'What is your source folder?'
-$dest = Read-Host -Prompt 'What is your destination folder?'
+    $validpath = $false
+    while(!$validpath) {
+        $dest = Read-Host -Prompt $prompt2
+        if (Test-Path $dest)
+        {
+            $validpath = $true
+        }
+    }
 
-$SourceDirectory = $PWD.Path+"\"+$src
-$DestinationDirectory = $PWD.Path+"\"+$dest
-$ExcludeSubDirectory = $SourceDirectory+'\.git'
-$files = Get-ChildItem $SourceDirectory -Recurse | Where-Object { $ExcludeSubDirectory -notcontains $_.DirectoryName }
-
-foreach ($file in $files)
-{
-	$CopyPath = Join-Path $DestinationDirectory $file.FullName.Substring($SourceDirectory.length)
-	Copy-Item $file.FullName -Destination $CopyPath
-}
+    $ExcludeSubDirectory = $src+'\.git'
+    $files = Get-ChildItem $src -Recurse | Where-Object { $ExcludeSubDirectory -notcontains $_.DirectoryName }
+    Write-Host 'Start copying files (except files in .git directory) from '$src 'to'$dest'...' -ForegroundColor Yellow
+    foreach ($file in $files)
+    {
+	    $CopyPath = Join-Path $DestinationDirectory $file.FullName.Substring($SourceDirectory.length)
+	    Copy-Item $file.FullName -Destination $CopyPath
+    }
 }
 
 #ProjectTemplate --> GroupProjectTemplate
-TDSP_local_copy
+TDSP_local_copy 'ProjectTemplate repository from Microsoft TDSP team' 'Your GroupProjectTemplate repository'
 #Utilities --> GroupUtilities
-TDSP_local_copy
+TDSP_local_copy 'Utilities repository from Microsoft TDSP team' 'Your GroupUtilities repository'
 
