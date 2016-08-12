7a78b239b5814b03d5cb2572667eb7fb521ad29f
inchiosa <inchiosa@users.noreply.github.com>
2016-08-11 21:12:01 -0700

    Add downloading of kddverification.r

--- Misc/KDDCup2016/Scripts/verification.sh
+++ Misc/KDDCup2016/Scripts/verification.sh
@@ -5,4 +5,10 @@ whoami > /tmp/verif/whoami
 pwd > /tmp/verif/pwd
 echo $amlAuth > /tmp/verif/amlAuth
 
+cd /home/remoteuser
+
+if [[ -f kddverification.r ]]; then sudo rm -Rf kddverification.r; fi;
+
+wget https://raw.githubusercontent.com/Azure/Azure-MachineLearning-DataScience/master/Misc/KDDCup2016/Scripts/kddverification.r
+
 Rscript --default-packages= /home/remoteuser/kddverification.r $amlAuth > /tmp/verif/rout.log 2> /tmp/verif/rerr.log
