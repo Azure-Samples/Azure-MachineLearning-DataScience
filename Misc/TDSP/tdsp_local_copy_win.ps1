e2c596b1a7de4739e935aad0b907735e876ef0b5
hangzh-msft <hangzh@microsoft.com>
2016-08-24 23:47:38 -0700

    Update tdsp_local_copy_win.ps1
    Team lead does not need to seed his team utilities from group utilities.

--- Misc/TDSP/tdsp_local_copy_win.ps1
+++ Misc/TDSP/tdsp_local_copy_win.ps1
@@ -57,11 +57,11 @@ function select_your_role ($role) {
 	elseif ($role -eq 2){
 		$reponame1 = 'Your GroupProjectTemplate repository'
 		$reponame2 = 'Your TeamProjectTemplate repository'
-		$reponame3 = 'Your GroupUtilities repository'
-		$reponame4 = 'Your TeampUtilities repository'
+		#$reponame3 = 'Your GroupUtilities repository'
+		#$reponame4 = 'Your TeampUtilities repository'
 
 		TDSP_local_copy $reponame1 $reponame2
-		TDSP_local_copy $reponame3 $reponame4
+		#TDSP_local_copy $reponame3 $reponame4
 	}
 	else{
 		$reponame1 = 'Your TeamProjectTemplate repository'
