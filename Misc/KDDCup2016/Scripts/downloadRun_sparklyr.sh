7583ecb0d162b112d14449caebb6bf7af6fd711a
Debraj GuhaThakurta <deguhath@users.noreply.github.com>
2016-09-17 15:48:24 -0700

    Delete downloadRun_sparklyr.sh

--- Misc/KDDCup2016/Scripts/downloadRun_sparklyr.sh
+++
@@ -1,9 +0,0 @@
-
-cd /home/remoteuser
-wget https://github.com/rstudio/sparklyr/archive/df9de9a5c52a89ae025483652d2c033162f185cd.zip -O sparklyr0801.zip
-unzip sparklyr0801.zip
-tar -czvf sparklyr0801.tar.gz sparklyr-df9de9a5c52a89ae025483652d2c033162f185cd
-
-sudo R --vanilla --quiet  <  /home/remoteuser/update_sparklyr.R
-
-
