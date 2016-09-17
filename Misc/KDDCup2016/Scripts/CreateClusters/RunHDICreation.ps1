d899ebf07e0766321daaf33110beaada5b03b8a8
Debraj GuhaThakurta <deguhath@users.noreply.github.com>
2016-09-17 09:42:19 -0700

    Add files via upload

--- Misc/KDDCup2016/Scripts/CreateClusters/RunHDICreation.ps1
+++ Misc/KDDCup2016/Scripts/CreateClusters/RunHDICreation.ps1
@@ -7,33 +7,31 @@ Login-AzureRmAccount
 # SET WORKING FOLDER PATH
 #######################################################################
 Set-Location -Path C:\Users\deguhath\Desktop\CDSP\Spark\KDDBlog\ProvisionScripts
+$basepath = Get-Location
+#######################################################################
+# READ IN CLUSTER CONFIGURATION PARAMETERS
+#######################################################################
+#READ IN PARAMETER CSV CONFIGURATION FILE
 $currentPathTmp = Get-Location
-$currentPath = [string]$currentPathTmp
-#######################################################################
-# READ IN PARAMETERS
-#######################################################################
-#READ IN PARAMETER CSV FILE
+$currentPath = [string]$basepath + "\Configuration"
 $paramFile = Import-Csv ClusterParameters.csv
 
-#GET ALL THE PARAMETERS FROM THE PARAMETER FILE
+# GET ALL THE PARAMETERS FROM THE CONFIGURATION PARAMETER FILE
 $scriptpathnametmp = $paramFile | Where-Object {$_.Parameter -eq "ScriptPath"} | % {$_.Value}
 $scriptpathname = [string]$scriptpathnametmp
 $subscriptionname = $paramFile | Where-Object {$_.Parameter -eq "SubscriptionName"} | % {$_.Value}
 $subscriptionid = $paramFile | Where-Object {$_.Parameter -eq "SubscriptionID"} | % {$_.Value}
 $tenantid = $paramFile | Where-Object {$_.Parameter -eq "TenantID"} | % {$_.Value}
 $resourcegroup = $paramFile | Where-Object {$_.Parameter -eq "ResourceGroup"} | % {$_.Value}
-$location = $paramFile | Where-Object {$_.Parameter -eq "Location"} | % {$_.Value}
-$sourcestoragename = $paramFile | Where-Object {$_.Parameter -eq "TenantID"} | % {$_.Value}
 $profilepathtmp = $paramFile | Where-Object {$_.Parameter -eq "Profilepath"} | % {$_.Value}
 $profilepath = $currentPath + "\" + [string]$profilePathTmp
 $tmp = $paramFile | Where-Object {$_.Parameter -eq "ClusterStartIndex"} | % {$_.Value}; $clusterstartindex = [int]$tmp;
 $tmp = $paramFile | Where-Object {$_.Parameter -eq "ClusterEndIndex"} | % {$_.Value}; $clusterendindex = [int]$tmp;
 $clusterprefix = $paramFile | Where-Object {$_.Parameter -eq "ClusterPrefix"} | % {$_.Value}
-$sourcedataresourcegroup = $paramFile | Where-Object {$_.Parameter -eq "SourceDataResourceGroup"} | % {$_.Value};
-$sourcedatastoragename = $paramFile | Where-Object {$_.Parameter -eq "SourceDataStorageName"} | % {$_.Value};
-$sourcedatacontainername = $paramFile | Where-Object {$_.Parameter -eq "SourceDataContainerName"} | % {$_.Value}
 $tmp = $paramFile | Where-Object {$_.Parameter -eq "ClusterInfoOutputFileName"} | % {$_.Value}; $clusterinfooutputfilename = $currentPath + "\" + [string]$tmp 
-$tmp = $paramFile | Where-Object {$_.Parameter -eq "SubmitClusterCreationFileName"} | % {$_.Value}; $submitclustercreationfilename = $currentPath + "\" + [string]$tmp 
+$tmp = $paramFile | Where-Object {$_.Parameter -eq "NumWorkerNodes"} | % {$_.Value}; $numworkernodes = [int]$tmp;
+$tmp = $paramFile | Where-Object {$_.Parameter -eq "AdminUsername"} | % {$_.Value}; $adminusername = [string]$tmp;
+$tmp = $paramFile | Where-Object {$_.Parameter -eq "SshUsername"} | % {$_.Value}; $sshusername = [string]$tmp;
 
 #######################################################################
 # SET AZURE SUBSCRIPTIONS & SAVE PROFILE INFORMATION IN A FILE
@@ -47,13 +45,16 @@ Save-AzureRmProfile -Path $profilepath
 #######################################################################
 for($i=$clusterstartindex; $i -le $clusterendindex; $i++) {
 	$clustername = $clusterprefix+$i;
+	# Generates a random number between 1 and 10000 and adds it to the clustername for the cluster password 
 	$randnum = Get-Random -minimum 1 -maximum 100001
 	$clusterpasswd = $clustername.substring(0,1).toupper() + $clustername.substring(1).tolower() + "_" + $randnum
+	
+	# Outputs the cluster login and password information to a file
 	Set-Location -Path $currentPath
 	$clustername,$clusterpasswd -join ',' | out-file -filepath $clusterinfooutputfilename -append -Width 200;
 	
 	## SPECIFY ABSOLUTE PATH TO THE SCRIPT THAT YOU WANT TO SUBMIT AND RUN IN PARALLEL 
-	Start-Job -ScriptBlock {C:\Users\deguhath\Desktop\CDSP\Spark\KDDBlog\ProvisionScripts\SubmitHDICreation.ps1 $args[0] $args[1] $args[2] $args[3]} -ArgumentList @($clustername, $resourcegroup, $profilepath, $clusterpasswd)
+	Start-Job -ScriptBlock {C:\Users\deguhath\Desktop\CDSP\Spark\KDDBlog\ProvisionScripts\CreateClusters\SubmitHDICreation.ps1 $args[0] $args[1] $args[2] $args[3] $args[4] $args[5] $args[6]} -ArgumentList @($clustername, $resourcegroup, $profilepath, $clusterpasswd, $numworkernodes, $adminusername, $sshusername)
 }
 #######################################################################
 # END
