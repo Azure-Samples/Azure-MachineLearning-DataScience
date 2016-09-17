700f1f0d5dbb47eca9b6e7d4dbe11b91898febb3
Debraj GuhaThakurta <deguhath@users.noreply.github.com>
2016-09-17 15:48:31 -0700

    Delete update_sparklyr.R

--- Misc/KDDCup2016/Scripts/update_sparklyr.R
+++
@@ -1,5 +0,0 @@
-.libPaths("/usr/lib64/microsoft-r/8.0/lib64/R/library")
-setwd("/usr/lib64/microsoft-r/8.0/lib64/R/library")
-
-remove.packages("sparklyr")
-install.packages("/home/remoteuser/sparklyr0801.tar.gz", repos=NULL, type="source")
