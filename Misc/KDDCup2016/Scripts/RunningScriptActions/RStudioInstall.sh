19f72348c85378898027a5930c23eb059a21aead
Debraj GuhaThakurta <deguhath@users.noreply.github.com>
2016-09-18 01:06:09 -0700

    Delete RStudioInstall.sh

--- Misc/KDDCup2016/Scripts/RunningScriptActions/RStudioInstall.sh
+++
@@ -1,4 +0,0 @@
-sudo cd /home/remoteuser
-sudo wget http://mrsactionscripts.blob.core.windows.net/rstudio-server-community-v01/InstallRStudio.sh
-sudo chmod 755 InstallRStudio.sh
-sudo ./InstallRStudio.sh
