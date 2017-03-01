345413db3562e36a6799acb9d76b099aabe06019
Debraj GuhaThakurta <deguhath@users.noreply.github.com>
2017-03-01 10:10:13 -0800

    Update DSVM_Customization_Script_New2.sh

--- Misc/StrataSanJose2017/Scripts/DSVM_Customization_Script_New2.sh
+++ Misc/StrataSanJose2017/Scripts/DSVM_Customization_Script_New2.sh
@@ -1,9 +1,9 @@
-cd /etc/skel
+cd /home/remoteuser
 mkdir  Data Code
 mkdir Code/MRS Code/sparklyr Code/SparkR
 
 ## DOWNLOAD ALL CODE FILES
-cd /etc/skel
+cd /home/remoteuser
 cd Code/MRS
 wget https://raw.githubusercontent.com/Azure/Azure-MachineLearning-DataScience/master/Misc/StrataSanJose2017/Code/MRS/1-Clean-Join-Subset.r
 wget https://raw.githubusercontent.com/Azure/Azure-MachineLearning-DataScience/master/Misc/StrataSanJose2017/Code/MRS/2-Train-Test-Subset.r
@@ -11,12 +11,12 @@ wget https://raw.githubusercontent.com/Azure/Azure-MachineLearning-DataScience/m
 wget https://raw.githubusercontent.com/Azure/Azure-MachineLearning-DataScience/master/Misc/StrataSanJose2017/Code/MRS/SetComputeContext.r
 wget https://raw.githubusercontent.com/Azure/Azure-MachineLearning-DataScience/master/Misc/StrataSanJose2017/Code/MRS/azureml-settings.json
 
-cd /etc/skel
+cd /home/remoteuser
 cd Code/SparkR
 wget https://raw.githubusercontent.com/Azure/Azure-MachineLearning-DataScience/master/Misc/StrataSanJose2017/Code/SparkR/SparkR_NYCTaxi_forDSVM.Rmd
 wget https://raw.githubusercontent.com/Azure/Azure-MachineLearning-DataScience/master/Misc/StrataSanJose2017/Code/SparkR/SparkR_NYCTaxi_forDSVM.html
 
-cd /etc/skel
+cd /home/remoteuser
 cd Code/sparklyr
 wget https://raw.githubusercontent.com/Azure/Azure-MachineLearning-DataScience/master/Misc/StrataSanJose2017/Code/sparklyr/sparklyr_NYCTaxi_forDSVM.Rmd
 wget https://raw.githubusercontent.com/Azure/Azure-MachineLearning-DataScience/master/Misc/StrataSanJose2017/Code/sparklyr/sparklyr_NYCTaxi_forDSVM.html
