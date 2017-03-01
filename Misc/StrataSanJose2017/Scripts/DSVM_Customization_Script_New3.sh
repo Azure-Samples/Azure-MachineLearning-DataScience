8a42530ca2c5cd1a462b3ede91c2129561fa09fb
Debraj GuhaThakurta <deguhath@users.noreply.github.com>
2017-03-01 13:02:49 -0800

    Update DSVM_Customization_Script_New3.sh

--- Misc/StrataSanJose2017/Scripts/DSVM_Customization_Script_New3.sh
+++ Misc/StrataSanJose2017/Scripts/DSVM_Customization_Script_New3.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source /etc/profile.d/hadoop.sh
 
 cat /opt/hadoop/.ssh/id_rsa.pub >> /opt/hadoop/.ssh/authorized_keys
 chmod 0600 /opt/hadoop/.ssh/authorized_keys
@@ -7,9 +8,9 @@ chown hadoop /opt/hadoop/.ssh/authorized_keys
 systemctl start hadoop-namenode hadoop-datanode hadoop-yarn rstudio-server
 
 cd /home/remoteuser
-hadoop fs -mkdir /user/RevoShare/rserve2
-hadoop fs -mkdir /user/RevoShare/rserve2/Predictions
-hadoop fs -chmod -R 777 /user/RevoShare/rserve2
-hadoop fs -mkdir /user/RevoShare/remoteuser
-hadoop fs -mkdir /user/RevoShare/remoteuser/Data
-hadoop fs -mkdir /user/RevoShare/remoteuser/Models
+/opt/hadoop/current/bin/hadoop fs -mkdir /user/RevoShare/rserve2
+/opt/hadoop/current/bin/hadoop fs -mkdir /user/RevoShare/rserve2/Predictions
+/opt/hadoop/current/bin/hadoop fs -chmod -R 777 /user/RevoShare/rserve2
+/opt/hadoop/current/bin/hadoop fs -mkdir /user/RevoShare/remoteuser
+/opt/hadoop/current/bin/hadoop fs -mkdir /user/RevoShare/remoteuser/Data
+/opt/hadoop/current/bin/hadoop fs -mkdir /user/RevoShare/remoteuser/Models
