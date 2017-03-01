9dad79ceedd42332413cf1c032ce9c02bf1f2b6f
Debraj GuhaThakurta <deguhath@users.noreply.github.com>
2017-03-01 12:39:15 -0800

    Update DSVM_Customization_Script_New3.sh

--- Misc/StrataSanJose2017/Scripts/DSVM_Customization_Script_New3.sh
+++ Misc/StrataSanJose2017/Scripts/DSVM_Customization_Script_New3.sh
@@ -7,4 +7,9 @@ chown hadoop /opt/hadoop/.ssh/authorized_keys
 systemctl start hadoop-namenode hadoop-datanode hadoop-yarn rstudio-server
 
 cd /home/remoteuser
-
+hadoop fs -mkdir /user/RevoShare/rserve2
+hadoop fs -mkdir /user/RevoShare/rserve2/Predictions
+hadoop fs -chmod -R 777 /user/RevoShare/rserve2
+hadoop fs -mkdir /user/RevoShare/remoteuser
+hadoop fs -mkdir /user/RevoShare/remoteuser/Data
+hadoop fs -mkdir /user/RevoShare/remoteuser/Models
