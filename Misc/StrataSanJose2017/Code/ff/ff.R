d723ded7ee905bb40e5114851950a87f537176c3
Katherine Zhao <mezmicrosoft@users.noreply.github.com>
2017-03-08 11:54:55 -0500

    Add files via upload

---
+++ Misc/StrataSanJose2017/Code/ff/ff.R
@@ -0,0 +1,43 @@
+# 
+# DESCRIPTION
+# 
+# This script shows an example of how to create and use ffdf objects
+# with the "ff" and its related packages.
+#
+
+# install "ff" and related packages:
+if(!require("ff")) install.packages('ff')
+if(!require("ffbase")) install.packages('ffbase')
+if(!require("biglm")) install.packages('biglm')
+
+
+##### Example of "ff"
+
+# call the library
+library("ff")
+
+# read airline data into a ffdf object (20 million rows * 26 columns; 1.5GB)
+# (it takes 2 mins~; depends on machine)
+airline_ff <- read.csv.ffdf(file = "http://strata2017r.blob.core.windows.net/airline/airline_20MM.csv",
+                            header = TRUE,
+                            na.strings = NA)
+head(airline_ff)
+
+# size of ffdf object in memory = 87960 bytes/88 KB
+object.size(airline_ff)
+
+
+##### Example of "ffbase"
+
+# call the library
+library("ffbase")
+library("biglm")
+
+# perform simply data transformation on "CRSDepTime"
+# round "CRSDepTime" to the nearest hour
+airline_ff[, "CRSDepTime"] <- floor(airline_ff[, "CRSDepTime"] / 100)
+
+# fit a glm model
+model_ff <- bigglm.ffdf(formula = IsArrDelayed ~ Month+DayofMonth+DayOfWeek+CRSDepTime+Distance, 
+                        data = airline_ff, family = binomial())
+summary(model_ff)
