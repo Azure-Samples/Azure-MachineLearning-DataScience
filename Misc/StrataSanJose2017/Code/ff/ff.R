328c0c6cf4f11acf7eacc977a769358d351bd65c
Katherine Zhao <mezmicrosoft@users.noreply.github.com>
2017-03-08 21:54:26 -0500

    Add files via upload

--- Misc/StrataSanJose2017/Code/ff/ff.R
+++ Misc/StrataSanJose2017/Code/ff/ff.R
@@ -20,7 +20,7 @@ setwd("/home/remoteuser/Data")
 library("ff")
 
 # read airline data into a ffdf object (20 million rows * 26 columns; 1.5GB)
-# (it takes 2 mins~; depends on machine)
+# (it takes 2+ mins~)
 airline_ff <- read.csv.ffdf(file = "airline_20MM.csv",
                             header = TRUE,
                             na.strings = NA)
@@ -41,6 +41,7 @@ library("biglm")
 airline_ff[, "CRSDepTime"] <- floor(airline_ff[, "CRSDepTime"] / 100)
 
 # fit a glm model
+# (it takes 2+ mins)
 model_ff <- bigglm.ffdf(formula = IsArrDelayed ~ Month+DayofMonth+DayOfWeek+CRSDepTime+Distance, 
                         data = airline_ff, family = binomial())
 summary(model_ff)
