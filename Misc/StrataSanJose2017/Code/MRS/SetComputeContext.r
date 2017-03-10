572dbb4af8774a1279948a1c181d29728080995a
inchiosa <inchiosa@users.noreply.github.com>
2017-03-09 22:11:07 -0800

    Update SetComputeContext.r

--- Misc/StrataSanJose2017/Code/MRS/SetComputeContext.r
+++ Misc/StrataSanJose2017/Code/MRS/SetComputeContext.r
@@ -1,16 +1,16 @@
 if(file.exists("/dsvm"))
 {
   Sys.setenv(SPARK_HOME="/dsvm/tools/spark/current",
-    YARN_CONF_DIR="/opt/hadoop/current/etc/hadoop", 
-    JAVA_HOME = "/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.111-1.b15.el7_2.x86_64",
-    PATH="/anaconda/envs/py35/bin:/dsvm/tools/cntk/cntk/bin:/usr/local/mpi/bin:/dsvm/tools/spark/current/bin:/anaconda/envs/py35/bin:/dsvm/tools/cntk/cntk/bin:/usr/local/mpi/bin:/dsvm/tools/spark/current/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/opt/hadoop/current/sbin:/opt/hadoop/current/bin:/home/remoteuser/.local/bin:/home/remoteuser/bin:/opt/hadoop/current/sbin:/opt/hadoop/current/bin"
+             YARN_CONF_DIR="/opt/hadoop/current/etc/hadoop", 
+             JAVA_HOME = "/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.111-1.b15.el7_2.x86_64",
+             PATH="/anaconda/envs/py35/bin:/dsvm/tools/cntk/cntk/bin:/usr/local/mpi/bin:/dsvm/tools/spark/current/bin:/anaconda/envs/py35/bin:/dsvm/tools/cntk/cntk/bin:/usr/local/mpi/bin:/dsvm/tools/spark/current/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/opt/hadoop/current/sbin:/opt/hadoop/current/bin:/home/remoteuser/.local/bin:/home/remoteuser/bin:/opt/hadoop/current/sbin:/opt/hadoop/current/bin"
   )
 }
 
 useHDFS <- TRUE
 
 if(useHDFS) {
-
+  
   ################################################
   # Use Hadoop-compatible Distributed File System
   # N.B. Can be used with local or RxSpark compute contexts
@@ -21,7 +21,7 @@ if(useHDFS) {
   dataDir <- "/user/RevoShare/remoteuser/Data"
   
   ################################################
-
+  
   if(rxOptions()$hdfsHost == "default") {
     fullDataDir <- dataDir
   } else {
@@ -33,7 +33,7 @@ if(useHDFS) {
   # Use Native, Local File System
   # N.B. Can only be used with local compute context
   ################################################
-
+  
   rxOptions(fileSystem = RxNativeFileSystem())
   
   dataDir <- file.path(getwd(), "delayDataLarge")
@@ -47,18 +47,28 @@ if(useHDFS) {
 ################################################
 
 startRxSpark <- function() {
-  rxSparkConnect(reset = T,
-    consoleOutput=TRUE, numExecutors = 1, executorCores=2, 
-    executorMem="1g")
+  if (useHDFS) {
+    # When running on an HDInsight cluster,
+    # specifying numExecutors, executorCores,
+    # and executorMem is optional
+    rxSparkConnect(reset = T,
+                   consoleOutput = TRUE, 
+                   numExecutors = 1, 
+                   executorCores = 2, 
+                   executorMem = "1g"
+    )
+  } else {
+    cat("Using local compute context to process local data.\n")
+  }
 }
 
 rxRoc <- function(...){
   previousContext <- rxSetComputeContext(RxLocalSeq())
-
+  
   # rxRoc requires local compute context
   roc <- RevoScaleR::rxRoc(...)
-
+  
   rxSetComputeContext(previousContext)
-
+  
   return(roc)
 }
