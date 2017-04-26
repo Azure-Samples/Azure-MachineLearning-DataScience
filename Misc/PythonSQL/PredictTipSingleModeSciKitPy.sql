7da45d2fdf68b6075f23a090deb5f3e12b863274
Debraj GuhaThakurta <deguhath@users.noreply.github.com>
2017-04-26 08:13:44 -0700

    Add files via upload

--- Misc/PythonSQL/PredictTipSingleModeSciKitPy.sql
+++ Misc/PythonSQL/PredictTipSingleModeSciKitPy.sql
@@ -1,7 +1,7 @@
 USE [TaxiNYC_Sample]
 GO
 
-/****** Object:  StoredProcedure [dbo].[PredictTipSingleModeSciKitPy]    Script Date: 4/25/2017 11:40:51 PM ******/
+/****** Object:  StoredProcedure [dbo].[PredictTipSingleModeSciKitPy]    Script Date: 4/26/2017 3:09:40 PM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -39,13 +39,17 @@ import pickle;
 import numpy;
 import pandas;
 
+# Load model and unserialize
 mod = pickle.loads(model)
 
+# Get features for scoring from input data
 X = InputDataSet[["passenger_count", "trip_distance", "trip_time_in_secs", "direct_distance"]]
 
+# Score data to get tip prediction probability as a list (of float)
 probList = []
 probList.append((mod.predict_proba(X)[0])[1])
 
+# Create output data frame
 OutputDataSet = pandas.DataFrame(data = probList, columns = ["predictions"])
 ',
                                   @input_data_1 = @inquery,
@@ -66,5 +70,6 @@ OutputDataSet = pandas.DataFrame(data = probList, columns = ["predictions"])
   WITH RESULT SETS ((Score float));
 
 END
+
 
 GO
