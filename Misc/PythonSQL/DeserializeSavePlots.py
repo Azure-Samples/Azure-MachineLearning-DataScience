f96d2343f771e67a088917023a0333476b829721
xibingaomsft <xibingao@microsoft.com>
2017-05-02 15:16:52 -0700

    add dir info

--- Misc/PythonSQL/DeserializeSavePlots.py
+++ Misc/PythonSQL/DeserializeSavePlots.py
@@ -3,9 +3,10 @@ import pickle
 import os
 cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER={SERVER_NAME};DATABASE={DB_NAME};UID={USER_NAME};PWD={PASSWORD}')
 cursor = cnxn.cursor()
-cursor.execute("EXECUTE [nyctaxi1].[dbo].[SerializePlots]")
+cursor.execute("EXECUTE [dbo].[SerializePlots]")
 tables = cursor.fetchall()
 for i in range(0, len(tables)):
     fig = pickle.loads(tables[i][0])
-    fig.savefig(str(i)+'.png')
+    fig.savefig(str(i)+'.png');
+
 print("The plots are saved in directory: ",os.getcwd())
