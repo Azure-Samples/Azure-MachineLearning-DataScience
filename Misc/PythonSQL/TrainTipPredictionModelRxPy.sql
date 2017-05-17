710d247130ea1f13df7c992374d0ad32df5e65c3
Wei Guo <guowei123@gmail.com>
2017-05-17 16:00:37 -0700

    corrected typos

--- Misc/PythonSQL/TrainTipPredictionModelRxPy.sql
+++ Misc/PythonSQL/TrainTipPredictionModelRxPy.sql
@@ -1,4 +1,5 @@
-/****** Object:  StoredProcedure [dbo].[TrainTipPredictionModelSciKitPy]    Script Date: 4/25/2017 11:36:11 PM ******/
+/****** Object:  StoredProcedure [dbo].[TrainTipPredictionModelSciKitPy]    Script Date: 5/17/2017 11:36:11 PM ******/
+
 USE [TaxiNYC_Sample]
 GO
 
@@ -7,19 +8,6 @@ GO
 
 SET QUOTED_IDENTIFIER ON
 GO
-
-
---Split whole data into 60% training and 40% testing
-DROP TABLE IF EXISTS dbo.nyctaxi_sample_training
-SELECT * into nyctaxi_sample_training 
-FROM nyctaxi_sample
-  WHERE (ABS(CAST((BINARY_CHECKSUM(*) *RAND(98052)) as int)) % 100) < 60
-
-DROP TABLE IF EXISTS dbo.nyctaxi_sample_testing
-SELECT * into nyctaxi_sample_testing 
-FROM nyctaxi_sample
-WHERE (ABS(CAST((BINARY_CHECKSUM(*) *RAND(98052)) as int)) % 100) > 60
-
 
 DROP PROCEDURE IF EXISTS TrainTipPredictionModelRxPy;
 GO
@@ -54,15 +42,6 @@ END;
 GO
 
 --Save model to table
-DROP TABLE IF EXISTS nyc_taxi_models;
-GO
-CREATE TABLE nyc_taxi_models (
-	name VARCHAR(30) NOT NULL DEFAULT('default model') PRIMARY KEY,
-	model VARBINARY(MAX) NOT NULL
-);
-GO
-
-TRUNCATE TABLE nyc_taxi_models;
 
 DECLARE @model VARBINARY(MAX);
 EXEC TrainTipPredictionModelRxPy @model OUTPUT;
