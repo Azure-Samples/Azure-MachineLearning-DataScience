5b83e531c9ee9135ccfd78737725c0c9dc9fb624
WeiGuo <weig@microsoft.com>
2017-05-17 11:53:04 -0700

    wei updated train test split, sklearn predict, add rx train and predict

--- Misc/PythonSQL/PredictTipSciKitPy.sql
+++ Misc/PythonSQL/PredictTipSciKitPy.sql
@@ -9,21 +9,23 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 
-CREATE PROCEDURE [dbo].[PredictTipSciKitPy] @inquery nvarchar(max)
+DROP PROCEDURE IF EXISTS PredictTipSciKitPy;
+GO
+
+CREATE PROCEDURE [dbo].[PredictTipSciKitPy] (@model varchar(100))
 AS
 BEGIN
+  DECLARE @lmodel2 varbinary(max) = (select model from nyc_taxi_models where name = @model);
 
-  DECLARE @lmodel2 varbinary(max) = (SELECT TOP 1
-    model
-  FROM nyc_taxi_models);
-  EXEC sp_execute_external_script @language = N'Python',
-                                  @script = N'
+  EXEC sp_execute_external_script 
+	@language = N'Python',
+    @script = N'
 import pickle;
 import numpy;
 import pandas;
 from sklearn import metrics
 
-mod = pickle.loads(model)
+mod = pickle.loads(lmodel2)
 
 X = InputDataSet[["passenger_count", "trip_distance", "trip_time_in_secs", "direct_distance"]]
 y = numpy.ravel(InputDataSet[["tipped"]])
@@ -31,20 +33,26 @@ y = numpy.ravel(InputDataSet[["tipped"]])
 probArray = mod.predict_proba(X)
 probList = []
 for i in range(len(probArray)):
-    probList.append((probArray[i])[1])
+	probList.append((probArray[i])[1])
 
 probArray = numpy.asarray(probList)
 fpr, tpr, thresholds = metrics.roc_curve(y, probArray)
 aucResult = metrics.auc(fpr, tpr)
-print ("AUC is: " + str(aucResult))
+print ("AUC on testing data is: " + str(aucResult))
 
 OutputDataSet = pandas.DataFrame(data = probList, columns = ["predictions"])
-',
-                                  @input_data_1 = @inquery,
-                                  @params = N'@model varbinary(max)',
-                                  @model = @lmodel2
+',	
+	@input_data_1 = N'select tipped, fare_amount, passenger_count, trip_time_in_secs, trip_distance,
+					dbo.fnCalculateDistance(pickup_latitude, pickup_longitude, dropoff_latitude, dropoff_longitude) as direct_distance
+					from nyctaxi_sample_testing
+					',
+	@input_data_1_name = N'InputDataSet',
+	@params = N'@lmodel2 varbinary(max)',
+	@lmodel2 = @lmodel2
   WITH RESULT SETS ((Score float));
 
 END
-
 GO
+
+--Call stored procedure
+EXEC [dbo].[PredictTipSciKitPy] 'linear_model';  
--- Misc/PythonSQL/TrainTipPredictionModelSciKitPy.sql
+++ Misc/PythonSQL/TrainTipPredictionModelSciKitPy.sql
@@ -9,40 +9,62 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE PROCEDURE [dbo].[TrainTipPredictionModelSciKitPy]
+--Split whole data into 60% training and 40% testing
+DROP TABLE IF EXISTS dbo.nyctaxi_sample_training
+SELECT * into nyctaxi_sample_training 
+FROM nyctaxi_sample
+  WHERE (ABS(CAST((BINARY_CHECKSUM(*) *RAND(98052)) as int)) % 100) < 60
 
+DROP TABLE IF EXISTS dbo.nyctaxi_sample_testing
+SELECT * into nyctaxi_sample_testing 
+FROM nyctaxi_sample
+WHERE (ABS(CAST((BINARY_CHECKSUM(*) *RAND(98052)) as int)) % 100) > 60
+
+
+DROP PROCEDURE IF EXISTS TrainTipPredictionModelRxPy;
+GO
+
+CREATE PROCEDURE [dbo].[TrainTipPredictionModelRxPy] (@trained_model varbinary(max) OUTPUT)
 AS
 BEGIN
-  DECLARE @inquery nvarchar(max) = N'
-	select tipped, fare_amount, passenger_count, trip_time_in_secs, trip_distance, 
-    dbo.fnCalculateDistance(pickup_latitude, pickup_longitude,  dropoff_latitude, dropoff_longitude) as direct_distance
-    from nyctaxi_sample
-    tablesample (70 percent) repeatable (98052)
-'
-  -- Insert the trained model into a database table
-  INSERT INTO nyc_taxi_models
-  EXEC sp_execute_external_script @language = N'Python',
-                                  @script = N'
-
-from sklearn.linear_model import LogisticRegression
+  EXEC sp_execute_external_script 
+    @language = N'Python',
+    @script = N'
 import numpy
 import pickle
 import pandas
+from revoscalepy.functions.RxLogit import rx_logit_ex;
+from revoscalepy.functions.RxPredict import rx_predict_ex;
 
-## Create model
-X = InputDataSet[["passenger_count", "trip_distance", "trip_time_in_secs", "direct_distance"]]
-y = numpy.ravel(InputDataSet[["tipped"]])
+logitObj = rx_logit_ex("tipped ~ passenger_count + trip_distance + trip_time_in_secs + direct_distance", data = InputDataSet);
 
-SKLalgo = LogisticRegression()
-logitObj = SKLalgo.fit(X, y)
-
-## Serialize model and put it in data frame
-trained_model = pandas.DataFrame(data = [pickle.dumps(logitObj)], columns = ["model"])
+## Serialize model
+trained_model = pickle.dumps(logitObj)
 ',
-                                  @input_data_1 = @inquery,
-                                  @output_data_1_name = N'trained_model'
+    @input_data_1 = N'
+	select tipped, fare_amount, passenger_count, trip_time_in_secs, trip_distance, 
+    dbo.fnCalculateDistance(pickup_latitude, pickup_longitude,  dropoff_latitude, dropoff_longitude) as direct_distance
+    from nyctaxi_sample_training
+	',
+	@input_data_1_name = N'InputDataSet',
+	@params = N'@trained_model varbinary(max) OUTPUT',
+	@trained_model = @trained_model OUTPUT;
   ;
-
-END
-
+END;
 GO
+
+--Save model to table
+DROP TABLE IF EXISTS nyc_taxi_models;
+GO
+CREATE TABLE nyc_taxi_models (
+	name VARCHAR(30) NOT NULL DEFAULT('default model') PRIMARY KEY,
+	model VARBINARY(MAX) NOT NULL
+);
+GO
+
+TRUNCATE TABLE nyc_taxi_models;
+
+DECLARE @model VARBINARY(MAX);
+EXEC TrainTipPredictionModelRxPy @model OUTPUT;
+
+INSERT INTO nyc_taxi_models (name, model) VALUES('rx_model', @model);
