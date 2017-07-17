ee98894200e51fbb08f87361c9183f010c0b79bc
Brett Cannon <brettcannon@users.noreply.github.com>
2017-07-17 12:04:57 -0700

    Fix naming and list construction

--- Misc/PythonSQL/PredictTipSingleModeRxPy.sql
+++ Misc/PythonSQL/PredictTipSingleModeRxPy.sql
@@ -40,17 +40,16 @@ from revoscalepy.functions.RxPredict import rx_predict_ex
 mod = pickle.loads(model)
 
 # Get features for scoring from input data
-X = InputDataSet[["passenger_count", "trip_distance", "trip_time_in_secs", "direct_distance"]]
+x = InputDataSet[["passenger_count", "trip_distance", "trip_time_in_secs", "direct_distance"]]
 
 # Score data to get tip prediction probability as a list (of float)
 
-probArray = rx_predict_ex(mod, X)
+prob_array = rx_predict_ex(mod, x)
 
-probList = []
-probList.append(probArray._results["tipped_Pred"])
+prob_list = [prob_array._results["tipped_Pred"]]
 
 # Create output data frame
-OutputDataSet = pandas.DataFrame(data=probList, columns=["predictions"])
+OutputDataSet = pandas.DataFrame(data=prob_list, columns=["predictions"])
 	',
 	@input_data_1 = @inquery,
 	@params = N'@model varbinary(max),@passenger_count int,@trip_distance float,
