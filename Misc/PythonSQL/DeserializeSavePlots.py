65acd55c20da6d5cb1e9de0ed3c523ade698df2e
Brett Cannon <brettcannon@users.noreply.github.com>
2017-07-17 12:01:27 -0700

    Dont use semi-colons

--- Misc/PythonSQL/DeserializeSavePlots.py
+++ Misc/PythonSQL/DeserializeSavePlots.py
@@ -8,6 +8,6 @@ cursor.execute("EXECUTE [dbo].[SerializePlots]")
 tables = cursor.fetchall()
 for i, table in enumerate(tables):
     fig = pickle.loads(table[0])
-    fig.savefig(str(i)+'.png');
+    fig.savefig(str(i)+'.png')
 
 print("The plots are saved in directory:", os.getcwd())
