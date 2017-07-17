fc37a90a8ae77aaed8f69eba19c8dd3218bbe94a
Brett Cannon <brettcannon@users.noreply.github.com>
2017-07-17 12:01:16 -0700

    Use enumerate()

--- Misc/PythonSQL/DeserializeSavePlots.py
+++ Misc/PythonSQL/DeserializeSavePlots.py
@@ -6,8 +6,8 @@ cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER={SERVER_NAME};DATABASE={DB_NAM
 cursor = cnxn.cursor()
 cursor.execute("EXECUTE [dbo].[SerializePlots]")
 tables = cursor.fetchall()
-for i in range(len(tables)):
-    fig = pickle.loads(tables[i][0])
+for i, table in enumerate(tables):
+    fig = pickle.loads(table[0])
     fig.savefig(str(i)+'.png');
 
 print("The plots are saved in directory:", os.getcwd())
