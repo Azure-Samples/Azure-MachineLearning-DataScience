6e4b0384fab7a6ed2020800c1d1208bc650ca00c
hangzh-msft <hangzh@microsoft.com>
2017-08-01 17:36:47 -0700

    Created downloading ipynbs ps1 scripts for in db R

---
+++ Misc/KDD2017MRS/download_ipynbs.ps1
@@ -0,0 +1,2 @@
+wget https://raw.githubusercontent.com/Azure/Azure-MachineLearning-DataScience/master/Misc/KDD2017MRS/Prepare%20Jupyter%20Notebooks.ipynb -OutFile "Prepare Jupyter Notebooks.ipynb"
+wget https://raw.githubusercontent.com/Azure/Azure-MachineLearning-DataScience/master/Misc/KDD2017MRS/SQL_R_Services_End_to_End_Tutorial.ipynb -OutFile SQL_R_Services_End_to_End_Tutorial.ipynb
